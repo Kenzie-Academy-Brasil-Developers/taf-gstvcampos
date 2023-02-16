aab3d47bf6d77d50b41e8a0a1211a9b54b714ccf
Gustavo Campos Rodrigues <gustavo.bmc76@gmail.com>
2023-02-16 11:37:12 -0300

    certo

--- script.js
+++ script.js
@@ -1,48 +1,50 @@
 // Variáveis para aplicar os resultados do teste. Não as altere.
 
-// Sexo
-const gender = process.argv[2]; 
-// Altura
-const height = process.argv[3]; 
-// Repetições com barra
-const barReps = process.argv[4];
-// Tempo das repetições com barra 
-const barSeconds = process.argv[5]; 
-// Abdominais
-const abs = process.argv[6];
-// Corrida de 3km
-const run3km = process.argv[7]; 
-// Corrida de 5km
-const run5km = process.argv[8];
-// Natação 
-const swim = process.argv[9]; 
-// Mergulho
-const dive = process.argv[10]; 
+const gender = process.argv[2]; // Sexo 
+const height = process.argv[3]; // Altura
+const barReps = process.argv[4]; // Repetições com barra
+const barSeconds = process.argv[5]; // Tempo das repetições com barra 
+const abs = process.argv[6]; // Abdominais
+const runDistance = process.argv[7]; // Distância da corrida
+const runTime = process.argv[8]; // Tempo da corrida
+const swimDistance = process.argv[9]; // Distância da natação
+const swimTime = process.argv[10]; // Tempo da natação
+const diveTime = process.argv[11]; // Tempo de mergulho
 
-// Variável para armazenar o resultado do TAF
 let passed = false;
 
 // Importante:
 // Se o sexo (gender) for masculino, utilize a palavra "male"
 // Se o sexo (gender) for feminino, utilize a palavra "female"
 
-// Dica para começar:
-// if (gender == "male") { 
-//    **sua lógica aqui** 
-// }
+// Sugestão para começar:
+// if (gender === "male")  
+// **sua lógica a partir daqui**
 
 if (gender == "male") {
-    if (height > 1.69 && (barReps > 5 || barSeconds < 14) &&
-    abs > 40 && (run3km < 13 || run5km < 21) &&
-    (swim <= 1 || dive < 30)) {
+    if (height >= 1.70 && //Pelo menos 1,70 altura
+    (barReps >= 6 || barSeconds <= 15) && //Pelo menos 6 repetições de barra ou 15 segundos de duração
+    abs >= 41 && //Pelo menos 41 abdominais
+    ((runDistance <= 3000 && runTime <= 720) || //3km em no máximo 12 min ou
+    (runDistance <= 5000 && runTime <= 1200)) && //5km em no máximo 20 min
+    ((swimDistance >= 100 && swimTime <= 60) || //⁠Pelo menos 100m em no máximo de 1 min
+    (diveTime <= 30))) { //30s de mergulho
         passed = true;
     }
 }
 
 if (gender == "female") {
-    if (height > 1.59 && (barReps > 4 || barSeconds < 13) &&
-    abs > 40 && (run3km < 16 || run5km < 23) &&
-    (swim <= 1 || dive < 30)) {
+    if (height >= 1.60 && //Pelo menos 1,60 altura
+    (barReps >= 5 || barSeconds <= 12) && //Pelo menos 5 repetições de barra ou 15 segundos de duração
+    abs >= 41 && //Pelo menos 41 abdominais
+    ((runDistance <= 4000 && runTime <= 900) || //4km em no máximo 15 min ou
+    (runDistance <= 6000 && runTime <= 1320)) && //6km em no máximo 22 min
+    ((swimDistance >= 100 && swimTime <= 60) || //⁠Pelo menos 100m em no máximo de 1 min
+    (diveTime <= 30))) { //30s de mergulho
         passed = true;
     }
 }
+
+// Deve conter apenas esse console.log no seu código.
+// Senão os testes não irão funcionar.
+console.log(passed);
--- test.sh
+++ test.sh
@@ -4,28 +4,30 @@
 test_cases=$(cat test_cases.txt)
 
 # Loop through each test case
+x=1
 while read -r test_case; do
   # Extract input values from the test case string
   gender=$(echo $test_case | awk '{print $1}')
   height=$(echo $test_case | awk '{print $2}')
-  bar_reps=$(echo $test_case | awk '{print $3}')
-  bar_seconds=$(echo $test_case | awk '{print $4}')
+  barReps=$(echo $test_case | awk '{print $3}')
+  barSeconds=$(echo $test_case | awk '{print $4}')
   abs=$(echo $test_case | awk '{print $5}')
-  run_3km=$(echo $test_case | awk '{print $6}')
-  run_5km=$(echo $test_case | awk '{print $7}')
-  swim=$(echo $test_case | awk '{print $8}')
-  dive=$(echo $test_case | awk '{print $9}')
-  expected_output=$(echo $test_case | awk '{print $10}')
+  runDistance=$(echo $test_case | awk '{print $6}')
+  runTime=$(echo $test_case | awk '{print $7}')
+  swimDistance=$(echo $test_case | awk '{print $8}')
+  swimTime=$(echo $test_case | awk '{print $9}')
+  diveTime=$(echo $test_case | awk '{print $10}')
+  expected_output=$(echo $test_case | awk '{print $11}')
 
   # Run the JavaScript code with the input values as arguments
-  output=$(node script.js $gender $height $bar_reps $bar_seconds $abs $run_3km $run_5km $swim $dive)
-
+  output=$(node script.js $gender $height $barReps $barSeconds $abs $runDistance $runTime $swimDistance $swimTime $diveTime)
   # Compare the output with the expected output
   if [ "$output" = "$expected_output" ]; then
     # Output result in green if the test passed
-    echo -e "\033[32mPASS\033[0m: $gender $height $bar_reps $bar_seconds $abs $run_3km $run_5km $swim $dive"
+    echo -e "$x- \033[32mPASS\033[0m: $gender $height $barReps $barSeconds $abs $runDistance $runTime $swimDistance $swimTime $diveTime"
   else
     # Output result in red if the test failed
-    echo -e "\033[31mFAIL\033[0m: $gender $height $bar_reps $bar_seconds $abs $run_3km $run_5km $swim $dive"
+    echo -e "$x- \033[31mFAIL\033[0m: $gender $height $barReps $barSeconds $abs $runDistance $runTime $swimDistance $swimTime $diveTime"
   fi
+  x=$(( $x + 1 ))
 done <<< "$test_cases"
